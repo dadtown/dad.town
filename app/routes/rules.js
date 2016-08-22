760417e982c4362b1898882cf38b93a63c0693a0
widd <widd@teamimmersion.net>
2016-08-22 02:43:39 -0700

    merge rules and irc page

--- app/routes/rules.js
+++
@@ -1,9 +0,0 @@
-var angular = require('angular');
-
-var rules = angular.module('dadtown.rules', ['ngRoute']);
-
-rules.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/rules', {
-        templateUrl: 'rules.html'
-    });
-}]);
